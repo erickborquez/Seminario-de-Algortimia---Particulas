63f3c58d2e9969922d83499b1c2c14f573e44ba6
ErickJoestar <erickborquez@gmail.com>
2020-11-12 21:44:22 -0700

    update

--- mainwindow.py
+++ mainwindow.py
@@ -1,4 +1,4 @@
-from PySide2.QtWidgets import QMainWindow, QFileDialog, QMessageBox
+from PySide2.QtWidgets import QMainWindow, QFileDialog, QMessageBox, QTableWidgetItem
 from PySide2.QtCore import Slot
 from ui_window import Ui_MainWindow
 from Particulas.particula import Particula
@@ -14,9 +14,74 @@ class MainWindow(QMainWindow):
         self.ui.add_start_pushButton.clicked.connect(self.click_add_start)
         self.ui.add_end_pushButton.clicked.connect(self.click_add_end)
         self.ui.print_pushButton.clicked.connect(self.click_show)
+
         self.ui.actionAbrir.triggered.connect(self.action_abrir_archivo)
         self.ui.actionGuardar.triggered.connect(self.action_guardar_archivo)
 
+        self.ui.show_button.clicked.connect(self.click_show_table)
+        self.ui.search_button.clicked.connect(self.click_search_table)
+
+    @Slot()
+    def click_show_table(self):
+        self.ui.table.setColumnCount(6)
+        headers = ["Id", "Origen X", "Origen Y",
+                   "Destino X", "Destino Y", "Distancia"]
+        self.ui.table.setHorizontalHeaderLabels(headers)
+        self.ui.table.setRowCount(len(self.particulas))
+
+        row = 0
+        for particula in self.particulas:
+            id_widget = QTableWidgetItem(particula.id)
+            origen_x_widget = QTableWidgetItem(str(particula.origen_x))
+            origen_y_widget = QTableWidgetItem(str(particula.origen_y))
+            destino_x_widget = QTableWidgetItem(str(particula.destino_x))
+            destino_y_widget = QTableWidgetItem(str(particula.destino_y))
+            distancia_widget = QTableWidgetItem(str(particula.distancia))
+
+            self.ui.table.setItem(row, 0, id_widget)
+            self.ui.table.setItem(row, 1, origen_x_widget)
+            self.ui.table.setItem(row, 2, origen_y_widget)
+            self.ui.table.setItem(row, 3, destino_x_widget)
+            self.ui.table.setItem(row, 4, destino_y_widget)
+            self.ui.table.setItem(row, 5, distancia_widget)
+
+            row += 1
+
+        print('Mostrar tabla')
+
+    @Slot()
+    def click_search_table(self):
+        id = self.ui.search_line.text()
+
+        for particula in self.particulas:
+            if id == particula.id:
+                self.ui.table.clear()
+                self.ui.table.setColumnCount(6)
+                headers = ["Id", "Origen X", "Origen Y",
+                           "Destino X", "Destino Y", "Distancia"]
+                self.ui.table.setHorizontalHeaderLabels(headers)
+                self.ui.table.setRowCount(1)
+                id_widget = QTableWidgetItem(particula.id)
+
+                origen_x_widget = QTableWidgetItem(str(particula.origen_x))
+                origen_y_widget = QTableWidgetItem(str(particula.origen_y))
+                destino_x_widget = QTableWidgetItem(str(particula.destino_x))
+                destino_y_widget = QTableWidgetItem(str(particula.destino_y))
+                distancia_widget = QTableWidgetItem(str(particula.distancia))
+
+                self.ui.table.setItem(0, 0, id_widget)
+                self.ui.table.setItem(0, 1, origen_x_widget)
+                self.ui.table.setItem(0, 2, origen_y_widget)
+                self.ui.table.setItem(0, 3, destino_x_widget)
+                self.ui.table.setItem(0, 4, destino_y_widget)
+                self.ui.table.setItem(0, 5, distancia_widget)
+
+                return
+
+        QMessageBox.warning(
+            self, "Atencion", f'La particula con id "{id}" no se ha encontrado')
+
+    @Slot()
     def click_show(self):
         # self.libreria.mostrar()
         self.ui.plainTextEdit.clear()
--- ui_window.py
+++ ui_window.py
@@ -17,101 +17,181 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(470, 479)
+        MainWindow.resize(598, 577)
         self.actionAbrir = QAction(MainWindow)
         self.actionAbrir.setObjectName(u"actionAbrir")
         self.actionGuardar = QAction(MainWindow)
         self.actionGuardar.setObjectName(u"actionGuardar")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.groupBox = QGroupBox(self.centralwidget)
+        self.gridLayout_3 = QGridLayout(self.centralwidget)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.tabWidget = QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tab = QWidget()
+        self.tab.setObjectName(u"tab")
+        self.gridLayout = QGridLayout(self.tab)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.groupBox = QGroupBox(self.tab)
         self.groupBox.setObjectName(u"groupBox")
-        self.groupBox.setGeometry(QRect(0, 0, 491, 441))
+        self.gridLayout_2 = QGridLayout(self.groupBox)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
         self.label = QLabel(self.groupBox)
         self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(20, 30, 67, 17))
+
+        self.gridLayout_2.addWidget(self.label, 0, 0, 1, 1)
+
         self.id_lineEdit = QLineEdit(self.groupBox)
         self.id_lineEdit.setObjectName(u"id_lineEdit")
-        self.id_lineEdit.setGeometry(QRect(90, 30, 113, 25))
-        self.label_2 = QLabel(self.groupBox)
-        self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(20, 60, 67, 17))
-        self.origin_x_lineEdit = QLineEdit(self.groupBox)
-        self.origin_x_lineEdit.setObjectName(u"origin_x_lineEdit")
-        self.origin_x_lineEdit.setGeometry(QRect(90, 60, 113, 25))
-        self.label_3 = QLabel(self.groupBox)
-        self.label_3.setObjectName(u"label_3")
-        self.label_3.setGeometry(QRect(20, 90, 67, 17))
-        self.origin_y_lineEdit = QLineEdit(self.groupBox)
-        self.origin_y_lineEdit.setObjectName(u"origin_y_lineEdit")
-        self.origin_y_lineEdit.setGeometry(QRect(90, 90, 113, 25))
-        self.label_4 = QLabel(self.groupBox)
-        self.label_4.setObjectName(u"label_4")
-        self.label_4.setGeometry(QRect(20, 120, 67, 17))
-        self.dest_x_lineEdit = QLineEdit(self.groupBox)
-        self.dest_x_lineEdit.setObjectName(u"dest_x_lineEdit")
-        self.dest_x_lineEdit.setGeometry(QRect(90, 120, 113, 25))
-        self.label_5 = QLabel(self.groupBox)
-        self.label_5.setObjectName(u"label_5")
-        self.label_5.setGeometry(QRect(20, 150, 67, 17))
-        self.dest_y_lineEdit = QLineEdit(self.groupBox)
-        self.dest_y_lineEdit.setObjectName(u"dest_y_lineEdit")
-        self.dest_y_lineEdit.setGeometry(QRect(90, 150, 113, 25))
-        self.label_6 = QLabel(self.groupBox)
-        self.label_6.setObjectName(u"label_6")
-        self.label_6.setGeometry(QRect(20, 180, 67, 17))
-        self.velocity_lineEdit = QLineEdit(self.groupBox)
-        self.velocity_lineEdit.setObjectName(u"velocity_lineEdit")
-        self.velocity_lineEdit.setGeometry(QRect(90, 180, 113, 25))
-        self.label_7 = QLabel(self.groupBox)
-        self.label_7.setObjectName(u"label_7")
-        self.label_7.setGeometry(QRect(20, 210, 67, 17))
-        self.green_lineEdit = QLineEdit(self.groupBox)
-        self.green_lineEdit.setObjectName(u"green_lineEdit")
-        self.green_lineEdit.setGeometry(QRect(90, 270, 113, 25))
-        self.label_8 = QLabel(self.groupBox)
-        self.label_8.setObjectName(u"label_8")
-        self.label_8.setGeometry(QRect(20, 240, 67, 17))
-        self.red_lineEdit = QLineEdit(self.groupBox)
-        self.red_lineEdit.setObjectName(u"red_lineEdit")
-        self.red_lineEdit.setGeometry(QRect(90, 240, 113, 25))
-        self.label_9 = QLabel(self.groupBox)
-        self.label_9.setObjectName(u"label_9")
-        self.label_9.setGeometry(QRect(20, 270, 67, 17))
-        self.blue_lineEdit = QLineEdit(self.groupBox)
-        self.blue_lineEdit.setObjectName(u"blue_lineEdit")
-        self.blue_lineEdit.setGeometry(QRect(90, 300, 113, 25))
-        self.label_10 = QLabel(self.groupBox)
-        self.label_10.setObjectName(u"label_10")
-        self.label_10.setGeometry(QRect(20, 300, 67, 17))
+
+        self.gridLayout_2.addWidget(self.id_lineEdit, 0, 1, 1, 1)
+
         self.plainTextEdit = QPlainTextEdit(self.groupBox)
         self.plainTextEdit.setObjectName(u"plainTextEdit")
-        self.plainTextEdit.setGeometry(QRect(210, 30, 251, 401))
-        self.layoutWidget = QWidget(self.groupBox)
-        self.layoutWidget.setObjectName(u"layoutWidget")
-        self.layoutWidget.setGeometry(QRect(20, 340, 181, 89))
-        self.verticalLayout = QVBoxLayout(self.layoutWidget)
+
+        self.gridLayout_2.addWidget(self.plainTextEdit, 0, 2, 11, 1)
+
+        self.label_2 = QLabel(self.groupBox)
+        self.label_2.setObjectName(u"label_2")
+
+        self.gridLayout_2.addWidget(self.label_2, 1, 0, 1, 1)
+
+        self.origin_x_lineEdit = QLineEdit(self.groupBox)
+        self.origin_x_lineEdit.setObjectName(u"origin_x_lineEdit")
+
+        self.gridLayout_2.addWidget(self.origin_x_lineEdit, 1, 1, 1, 1)
+
+        self.label_3 = QLabel(self.groupBox)
+        self.label_3.setObjectName(u"label_3")
+
+        self.gridLayout_2.addWidget(self.label_3, 2, 0, 1, 1)
+
+        self.origin_y_lineEdit = QLineEdit(self.groupBox)
+        self.origin_y_lineEdit.setObjectName(u"origin_y_lineEdit")
+
+        self.gridLayout_2.addWidget(self.origin_y_lineEdit, 2, 1, 1, 1)
+
+        self.label_4 = QLabel(self.groupBox)
+        self.label_4.setObjectName(u"label_4")
+
+        self.gridLayout_2.addWidget(self.label_4, 3, 0, 1, 1)
+
+        self.dest_x_lineEdit = QLineEdit(self.groupBox)
+        self.dest_x_lineEdit.setObjectName(u"dest_x_lineEdit")
+
+        self.gridLayout_2.addWidget(self.dest_x_lineEdit, 3, 1, 1, 1)
+
+        self.label_5 = QLabel(self.groupBox)
+        self.label_5.setObjectName(u"label_5")
+
+        self.gridLayout_2.addWidget(self.label_5, 4, 0, 1, 1)
+
+        self.dest_y_lineEdit = QLineEdit(self.groupBox)
+        self.dest_y_lineEdit.setObjectName(u"dest_y_lineEdit")
+
+        self.gridLayout_2.addWidget(self.dest_y_lineEdit, 4, 1, 1, 1)
+
+        self.label_6 = QLabel(self.groupBox)
+        self.label_6.setObjectName(u"label_6")
+
+        self.gridLayout_2.addWidget(self.label_6, 5, 0, 1, 1)
+
+        self.velocity_lineEdit = QLineEdit(self.groupBox)
+        self.velocity_lineEdit.setObjectName(u"velocity_lineEdit")
+
+        self.gridLayout_2.addWidget(self.velocity_lineEdit, 5, 1, 1, 1)
+
+        self.label_7 = QLabel(self.groupBox)
+        self.label_7.setObjectName(u"label_7")
+
+        self.gridLayout_2.addWidget(self.label_7, 6, 0, 1, 1)
+
+        self.label_8 = QLabel(self.groupBox)
+        self.label_8.setObjectName(u"label_8")
+
+        self.gridLayout_2.addWidget(self.label_8, 7, 0, 1, 1)
+
+        self.red_lineEdit = QLineEdit(self.groupBox)
+        self.red_lineEdit.setObjectName(u"red_lineEdit")
+
+        self.gridLayout_2.addWidget(self.red_lineEdit, 7, 1, 1, 1)
+
+        self.label_9 = QLabel(self.groupBox)
+        self.label_9.setObjectName(u"label_9")
+
+        self.gridLayout_2.addWidget(self.label_9, 8, 0, 1, 1)
+
+        self.green_lineEdit = QLineEdit(self.groupBox)
+        self.green_lineEdit.setObjectName(u"green_lineEdit")
+
+        self.gridLayout_2.addWidget(self.green_lineEdit, 8, 1, 1, 1)
+
+        self.label_10 = QLabel(self.groupBox)
+        self.label_10.setObjectName(u"label_10")
+
+        self.gridLayout_2.addWidget(self.label_10, 9, 0, 1, 1)
+
+        self.blue_lineEdit = QLineEdit(self.groupBox)
+        self.blue_lineEdit.setObjectName(u"blue_lineEdit")
+
+        self.gridLayout_2.addWidget(self.blue_lineEdit, 9, 1, 1, 1)
+
+        self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.add_start_pushButton = QPushButton(self.layoutWidget)
+        self.add_start_pushButton = QPushButton(self.groupBox)
         self.add_start_pushButton.setObjectName(u"add_start_pushButton")
 
         self.verticalLayout.addWidget(self.add_start_pushButton)
 
-        self.add_end_pushButton = QPushButton(self.layoutWidget)
+        self.add_end_pushButton = QPushButton(self.groupBox)
         self.add_end_pushButton.setObjectName(u"add_end_pushButton")
 
         self.verticalLayout.addWidget(self.add_end_pushButton)
 
-        self.print_pushButton = QPushButton(self.layoutWidget)
+        self.print_pushButton = QPushButton(self.groupBox)
         self.print_pushButton.setObjectName(u"print_pushButton")
 
         self.verticalLayout.addWidget(self.print_pushButton)
 
+
+        self.gridLayout_2.addLayout(self.verticalLayout, 10, 0, 1, 2)
+
+
+        self.gridLayout.addWidget(self.groupBox, 0, 0, 1, 1)
+
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName(u"tab_2")
+        self.gridLayout_4 = QGridLayout(self.tab_2)
+        self.gridLayout_4.setObjectName(u"gridLayout_4")
+        self.search_line = QLineEdit(self.tab_2)
+        self.search_line.setObjectName(u"search_line")
+
+        self.gridLayout_4.addWidget(self.search_line, 1, 0, 1, 1)
+
+        self.search_button = QPushButton(self.tab_2)
+        self.search_button.setObjectName(u"search_button")
+
+        self.gridLayout_4.addWidget(self.search_button, 1, 1, 1, 1)
+
+        self.show_button = QPushButton(self.tab_2)
+        self.show_button.setObjectName(u"show_button")
+
+        self.gridLayout_4.addWidget(self.show_button, 1, 2, 1, 1)
+
+        self.table = QTableWidget(self.tab_2)
+        self.table.setObjectName(u"table")
+
+        self.gridLayout_4.addWidget(self.table, 0, 0, 1, 3)
+
+        self.tabWidget.addTab(self.tab_2, "")
+
+        self.gridLayout_3.addWidget(self.tabWidget, 0, 0, 1, 1)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 470, 22))
+        self.menubar.setGeometry(QRect(0, 0, 598, 22))
         self.menuArchivo = QMenu(self.menubar)
         self.menuArchivo.setObjectName(u"menuArchivo")
         MainWindow.setMenuBar(self.menubar)
@@ -124,6 +204,9 @@ class Ui_MainWindow(object):
         self.menuArchivo.addAction(self.actionGuardar)
 
         self.retranslateUi(MainWindow)
+
+        self.tabWidget.setCurrentIndex(1)
+
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
@@ -148,11 +231,16 @@ class Ui_MainWindow(object):
         self.label_7.setText(QCoreApplication.translate("MainWindow", u"Color RGB", None))
         self.label_8.setText(QCoreApplication.translate("MainWindow", u"Rojo", None))
         self.label_9.setText(QCoreApplication.translate("MainWindow", u"Verde", None))
-        self.blue_lineEdit.setText("")
         self.label_10.setText(QCoreApplication.translate("MainWindow", u"Azul", None))
+        self.blue_lineEdit.setText("")
         self.add_start_pushButton.setText(QCoreApplication.translate("MainWindow", u"Agregar al inicio", None))
         self.add_end_pushButton.setText(QCoreApplication.translate("MainWindow", u"Agregar al final", None))
         self.print_pushButton.setText(QCoreApplication.translate("MainWindow", u"Imprimir", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("MainWindow", u"Agregar", None))
+        self.search_line.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Titulo del libro", None))
+        self.search_button.setText(QCoreApplication.translate("MainWindow", u"Buscar", None))
+        self.show_button.setText(QCoreApplication.translate("MainWindow", u"Mostrar", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("MainWindow", u"Tabla", None))
         self.menuArchivo.setTitle(QCoreApplication.translate("MainWindow", u"Archivo", None))
     # retranslateUi
 
